40544dce403bd3aca99d85298e0d362a0d9299f8
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-26 15:28:02 +0200

    MILSTONE 0 FATTA

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios';
+//IMPORTO HEADER E MAIN
+import AppHeader from './components/App.header.vue';
+import AppMain from './components/App.main.vue';
+
+//IMPORTO STORE
+import { store } from './store.js';
+
+export default {
+    components: {
+        AppHeader,
+        AppMain
+    },
+    data(){
+        return{
+          //CHIAMATA DELLO STORE
+            store
+        }
+    }
+}
+</script>
+<template>
+  <div>
+    <!-- HEADER -->
+    <AppHeader />
+    <!-- MAIN -->
+    <AppMain />
+  </div>
+</template>
+<style lang="scss">
+    @use './styles/generals.scss' as *;
+    @use './styles/partials/variables' as *;
+  
+</style>
---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { reactive } from 'vue'
+
+const store = reactive({
+    //API DEI FILM E SERIE TV 
+    apiUrl: '',
+})
+
+export default store 
